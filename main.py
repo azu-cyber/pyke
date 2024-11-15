fbaa170707febc5253b3a15ec8b7e802d47470cd
azu-cyber <avikalizhimomi@gmail.com>
2024-11-15 15:35:04 +0530

    Prime sieve

--- main.py
+++ main.py
@@ -10,3 +10,18 @@ if number > 1:
             print(number, "is a prime number")
 else:
      print(number, 'number is not a prime number')
+def SieveOfEratosthenes (num) :
+    prime = [True for i in range(num+1)]
+    p = 2
+    while (p * p <= num):
+         if (prime[p]== True):
+              for i in range(p * p, num+1, p):
+                   prime[i] = False
+         p += 1
+    for p in range(2, num+1):
+         if prime[p]:
+              print(p)
+num = int( input())
+print("Following are the prime numbers smaller"),
+print("than or equal to", num) 
+SieveOfEratosthenes(num)
