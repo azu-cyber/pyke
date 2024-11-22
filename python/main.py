eddcdc01d7ae80f32d2cc748a0aa7c97d466b721
azu-cyber <avikalizhimomi@gmail.com>
2024-11-22 18:29:20 +0530

    bit[play]

--- python/main.py
+++ python/main.py
@@ -7,6 +7,15 @@ def numberOfBits(n):
         else:
             zeros+=1
         n >>= 1
-    print("\n\nOnes = ",ones, "\nZeros ",zeros)
-    number = int(input("Enter your number : "))
-    numberOfBits(number)
+    print("\n\nOnes = ", ones, "\nZeros ", zeros)
+number = int(input("Enter your number : "))
+numberOfBits(number)
+    #project 2
+def setOrNot(number, n) :
+        if number & (1 << (n - 1)):
+            print ( "\nSET")
+        else:
+            print ( "\nNOT SET")
+number = int(input("Enter number : "))
+n = int(input("Enter bit number : "))
+setOrNot(number, n)
